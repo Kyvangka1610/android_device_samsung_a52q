4a8162c7cbad72a1a02f19f6c599b59cf769d53d
kyvangka1610 <kyvangka2002@gmail.com>
2023-06-21 14:56:58 +0700

    a52q: enable udfs animation & quick tap
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- evolution_a52q.mk
+++ evolution_a52q.mk
@@ -32,6 +32,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := evolution_a52q
