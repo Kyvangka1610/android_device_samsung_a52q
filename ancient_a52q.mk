30d1ed8702a4b38935fe2b24f6e2ada031d52b9e
kyvangka1610 <kyvangka2002@gmail.com>
2023-10-22 03:34:09 +0700

    bring up ancient
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- ancient_a52q.mk
+++ ancient_a52q.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
@@ -33,8 +33,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# ancient stuff
+ANCIENT_OFFICIAL := true
+EXTRA_UDFPS_ANIMATIONS := true
+
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_a52q
+PRODUCT_NAME := ancient_a52q
 PRODUCT_DEVICE := a52q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A525F
